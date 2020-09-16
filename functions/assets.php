964b69a80867f711f750590e31fdd9419c7cc69e
remox112358 <remox112358@gmail.com>
2020-09-16 17:25:39 +0400

    Add alerts adaptability

--- functions/assets.php
+++ functions/assets.php
@@ -5,5 +5,4 @@
  */
 function wpaa_assets() {
     wp_enqueue_style('wpaa-style', plugins_url('/assets/style.css', WPAA_PLUGIN));
-    wp_enqueue_style('wpaa-style-responsive', plugins_url('/assets/style-responsive.css', WPAA_PLUGIN));
 }
